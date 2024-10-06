3fdd2f9bb412f49449c20d035b9cae849f0d1ea8
Theo Browne <me@t3.gg>
2024-10-06 02:10:12 -0700

    fix og images

---
+++ src/app/(tools)/svg-to-png/opengraph-image.tsx
@@ -0,0 +1,68 @@
+import { ImageResponse } from "next/og";
+
+export const runtime = "edge";
+
+// Image metadata
+export const alt = "QuickPic";
+export const size = {
+  width: 1200,
+  height: 630,
+};
+
+export const contentType = "image/png";
+
+// Image generation
+export default async function Image() {
+  // Font
+  const interSemiBold = fetch(
+    new URL("./fonts/Inter-SemiBold.ttf", import.meta.url)
+  ).then((res) => res.arrayBuffer());
+
+  return new ImageResponse(
+    (
+      // ImageResponse JSX element
+      <div
+        style={{
+          fontSize: 160,
+          background: "black",
+          width: "100%",
+          height: "100%",
+          display: "flex",
+          textAlign: "center",
+          alignItems: "center",
+          justifyContent: "center",
+          color: "white",
+          fontFamily: "Inter, sans-serif",
+        }}
+      >
+        <div
+          style={{
+            display: "flex",
+            flexDirection: "column",
+            textAlign: "center",
+            alignItems: "center",
+            justifyContent: "center",
+            gap: 4,
+          }}
+        >
+          <span>SVG ➡️ PNG</span>
+          <span style={{ fontSize: 40 }}>The only simple SVG converter.</span>
+        </div>
+      </div>
+    ),
+    // ImageResponse options
+    {
+      // For convenience, we can re-use the exported opengraph-image
+      // size config to also set the ImageResponse's width and height.
+      ...size,
+      fonts: [
+        {
+          name: "Inter",
+          data: await interSemiBold,
+          style: "normal",
+          weight: 400,
+        },
+      ],
+    }
+  );
+}
---
+++ src/app/utils/og-generator.tsx
@@ -0,0 +1,77 @@
+import { ImageResponse } from "next/og";
+
+// Image metadata
+export const size = {
+  width: 1200,
+  height: 630,
+};
+
+// Image generation
+export async function GenerateImage(params: {
+  title: string;
+  description?: string;
+}) {
+  // Fonts
+  const interSemiBold = fetch(
+    new URL("../fonts/Inter-SemiBold.ttf", import.meta.url)
+  ).then((res) => res.arrayBuffer());
+  const interLight = fetch(
+    new URL("../fonts/Inter-Light.ttf", import.meta.url)
+  ).then((res) => res.arrayBuffer());
+
+  return new ImageResponse(
+    (
+      // ImageResponse JSX element
+      <div
+        style={{
+          fontSize: 160,
+          background: "black",
+          width: "100%",
+          height: "100%",
+          display: "flex",
+          textAlign: "center",
+          alignItems: "center",
+          justifyContent: "center",
+          color: "white",
+          fontFamily: "Inter, sans-serif",
+        }}
+      >
+        <div
+          style={{
+            display: "flex",
+            flexDirection: "column",
+            textAlign: "center",
+            alignItems: "center",
+            justifyContent: "center",
+            gap: 4,
+          }}
+        >
+          <span style={{ fontWeight: 600 }}>{params.title}</span>
+          <span style={{ fontSize: 40, fontWeight: 300 }}>
+            {params.description ?? "Quick and free tools for pictures."}
+          </span>
+        </div>
+      </div>
+    ),
+    // ImageResponse options
+    {
+      // For convenience, we can re-use the exported opengraph-image
+      // size config to also set the ImageResponse's width and height.
+      ...size,
+      fonts: [
+        {
+          name: "Inter",
+          data: await interSemiBold,
+          style: "normal",
+          weight: 600,
+        },
+        {
+          name: "Inter",
+          data: await interLight,
+          style: "normal",
+          weight: 300,
+        },
+      ],
+    }
+  );
+}
