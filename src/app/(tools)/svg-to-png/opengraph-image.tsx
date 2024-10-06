3e0e3ae6d337d463fea7631957f748c15d04d05b
Theo Browne <me@t3.gg>
2024-10-06 02:15:18 -0700

    standardize og images and formatting

--- src/app/(tools)/svg-to-png/opengraph-image.tsx
+++ src/app/(tools)/svg-to-png/opengraph-image.tsx
@@ -1,68 +1,19 @@
-import { ImageResponse } from "next/og";
+import { GenerateImage } from "@/app/utils/og-generator";
 
 export const runtime = "edge";
 
-// Image metadata
-export const alt = "QuickPic";
+export const alt = "SVG ➡️ PNG - QuickPic";
+export const contentType = "image/png";
+
 export const size = {
   width: 1200,
   height: 630,
 };
 
-export const contentType = "image/png";
-
 // Image generation
 export default async function Image() {
-  // Font
-  const interSemiBold = fetch(
-    new URL("./fonts/Inter-SemiBold.ttf", import.meta.url)
-  ).then((res) => res.arrayBuffer());
-
-  return new ImageResponse(
-    (
-      // ImageResponse JSX element
-      <div
-        style={{
-          fontSize: 160,
-          background: "black",
-          width: "100%",
-          height: "100%",
-          display: "flex",
-          textAlign: "center",
-          alignItems: "center",
-          justifyContent: "center",
-          color: "white",
-          fontFamily: "Inter, sans-serif",
-        }}
-      >
-        <div
-          style={{
-            display: "flex",
-            flexDirection: "column",
-            textAlign: "center",
-            alignItems: "center",
-            justifyContent: "center",
-            gap: 4,
-          }}
-        >
-          <span>SVG ➡️ PNG</span>
-          <span style={{ fontSize: 40 }}>The only simple SVG converter.</span>
-        </div>
-      </div>
-    ),
-    // ImageResponse options
-    {
-      // For convenience, we can re-use the exported opengraph-image
-      // size config to also set the ImageResponse's width and height.
-      ...size,
-      fonts: [
-        {
-          name: "Inter",
-          data: await interSemiBold,
-          style: "normal",
-          weight: 400,
-        },
-      ],
-    }
-  );
+  return await GenerateImage({
+    title: "SVG ➡️ PNG",
+    description: "The only simple SVG converter.",
+  });
 }
