210ec793a881aefa4dc615c0f51e49a4ae4e49db
Theo Browne <me@t3.gg>
2024-10-06 00:04:46 -0700

    fix layout

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -2,11 +2,11 @@ import { SVGTool } from "./svg-fixer";
 
 export default function Home() {
   return (
-    <div className="grid grid-rows-[20px_1fr_20px] items-center justify-items-center min-h-screen p-8 pb-20 gap-16 sm:p-20 font-[family-name:var(--font-geist-sans)]">
-      <main className="flex flex-col gap-8 row-start-2 items-center sm:items-start">
+    <div className="flex flex-col justify-between min-h-screen p-8 sm:p-20 font-[family-name:var(--font-geist-sans)]">
+      <main className="flex-grow flex flex-col items-center justify-center">
         <SVGTool />
       </main>
-      <footer className="row-start-3 text-center text-sm text-gray-500">
+      <footer className="text-center text-sm text-gray-500 mt-8">
         <a
           href="https://github.com/t3dotgg/quickpic"
           target="_blank"
