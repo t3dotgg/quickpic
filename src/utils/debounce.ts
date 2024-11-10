270f0a30e04f451240a8528271bfbbca2326581f
Himanshu Gunwant <hgunwant2312@gmail.com>
2024-11-10 09:50:35 +0530

    feat: add custom color picker

---
+++ src/utils/debounce.ts
@@ -0,0 +1,13 @@
+export const debounce = <F extends (...args: Parameters<F>) => ReturnType<F>>(
+  func: F,
+  waitFor: number
+) => {
+  let timeout: NodeJS.Timeout;
+
+  const debounced = (...args: Parameters<F>) => {
+    clearTimeout(timeout);
+    timeout = setTimeout(() => func(...args), waitFor);
+  };
+
+  return debounced;
+};
