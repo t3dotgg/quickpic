7affbc1d7438ac2b8b68a26bbdc2bdbf163597b7
Theo Browne <me@t3.gg>
2024-11-10 22:05:20 -0800

    refactor logic for file types, give better errors

--- src/components/shared/file-dropzone.tsx
+++ src/components/shared/file-dropzone.tsx
@@ -51,15 +51,24 @@ export const FileDropzone: React.FC<FileDropzoneProps> = ({
       const files = e.dataTransfer?.files;
       if (files && files.length > 0) {
         const droppedFile = files[0];
-        if (
-          droppedFile &&
-          (acceptedFileTypes.includes(droppedFile.type) ||
-            acceptedFileTypes.some((type) =>
-              droppedFile.name.toLowerCase().endsWith(type.replace("*", "")),
-            ))
-        ) {
-          setCurrentFile(droppedFile);
+
+        if (!droppedFile) {
+          alert("How did you do a drop with no files???");
+          throw new Error("No files dropped");
         }
+
+        if (
+          !acceptedFileTypes.includes(droppedFile.type) &&
+          !acceptedFileTypes.some((type) =>
+            droppedFile.name.toLowerCase().endsWith(type.replace("*", "")),
+          )
+        ) {
+          alert("Invalid file type. Please upload a supported file type.");
+          throw new Error("Invalid file");
+        }
+
+        // Happy path
+        setCurrentFile(droppedFile);
       }
     },
     [acceptedFileTypes, setCurrentFile],
