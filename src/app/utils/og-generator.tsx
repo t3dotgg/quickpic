05a161c088f0ddddcadd8069f01f7fe8b46cc93e
ad-archer <adarcher21@gmail.com>
2024-11-11 12:32:45 -0500

    simplify the OpenGraph

--- src/app/utils/og-generator.tsx
+++ src/app/utils/og-generator.tsx
@@ -1,77 +1,58 @@
 import { ImageResponse } from "next/og";
 
-// Image metadata
-export const size = {
-  width: 1200,
-  height: 630,
-};
-
-// Image generation
 export async function GenerateImage(params: {
   title: string;
   description?: string;
 }) {
-  // Fonts
-  const interSemiBold = fetch(
-    new URL("../fonts/Inter-SemiBold.ttf", import.meta.url),
-  ).then((res) => res.arrayBuffer());
-  const interLight = fetch(
-    new URL("../fonts/Inter-Light.ttf", import.meta.url),
-  ).then((res) => res.arrayBuffer());
-
   return new ImageResponse(
     (
-      // ImageResponse JSX element
       <div
         style={{
-          fontSize: 160,
-          background: "black",
+          background: "#000",
           width: "100%",
           height: "100%",
           display: "flex",
-          textAlign: "center",
+          flexDirection: "column",
           alignItems: "center",
           justifyContent: "center",
-          color: "white",
-          fontFamily: "Inter, sans-serif",
+          padding: "40px",
         }}
       >
         <div
           style={{
             display: "flex",
             flexDirection: "column",
-            textAlign: "center",
             alignItems: "center",
             justifyContent: "center",
-            gap: 4,
+            textAlign: "center",
           }}
         >
-          <span style={{ fontWeight: 600 }}>{params.title}</span>
-          <span style={{ fontSize: 40, fontWeight: 300 }}>
-            {params.description}
-          </span>
+          <h1
+            style={{
+              fontSize: "60px",
+              fontWeight: "bold",
+              color: "white",
+              marginBottom: "20px",
+            }}
+          >
+            {params.title}
+          </h1>
+          {params.description && (
+            <p
+              style={{
+                fontSize: "30px",
+                color: "#888",
+              }}
+            >
+              {params.description}
+            </p>
+          )}
         </div>
       </div>
     ),
-    // ImageResponse options
     {
-      // For convenience, we can re-use the exported opengraph-image
-      // size config to also set the ImageResponse's width and height.
-      ...size,
-      fonts: [
-        {
-          name: "Inter",
-          data: await interSemiBold,
-          style: "normal",
-          weight: 600,
-        },
-        {
-          name: "Inter",
-          data: await interLight,
-          style: "normal",
-          weight: 300,
-        },
-      ],
-    },
+      width: 1200,
+      height: 630,
+    }
   );
 }
