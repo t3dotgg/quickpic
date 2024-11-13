fbc5ffa67a52f31bd6fa59bc28a535d4f352e8f3
Khagesh Patel <khageshgopalpatel@gmail.com>
2024-11-14 00:21:42 +0530

    Added icon-generator page where user can generate icon in different sizes and download them all at once

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -26,6 +26,9 @@ export default function Home() {
         <Link href="/rounded-border" className="text-blue-500 hover:underline">
           Corner Rounder
         </Link>
+        <Link href="/icon-generator" className="text-blue-500 hover:underline">
+          Icon Generator
+        </Link>
       </main>
       <footer className="mt-8 text-center text-sm text-gray-500">
         <a
